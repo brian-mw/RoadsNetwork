ddfd8bd27950955ece287f39ed79fa3dc47875bb
bryan <brianmsyamboza@gmail.com>
2023-10-31 10:14:51 +0200

    refactor lib

--- lib.py
+++ lib.py
@@ -1,34 +1,21 @@
-class RoadNetwork(object):
-    def __init__(self, nodes, init_graph):
+class RoadNetwork:
+    def __init__(self, nodes, graph):
         self.nodes = nodes
-        self.graph = self.init_graph(nodes, init_graph)
-
-    def init_graph(self, nodes, init_graph):
-        graph = {}
-        for node in nodes:
-            graph[node] = {}
-
-        graph.update(init_graph)
-
-        for node, edges in graph.items():
-            for adjacent_node, value in edges.items():
-                if graph[adjacent_node].get(node, False) == False:
-                    graph[adjacent_node][node] = value
-
-        return graph
+        self.graph = graph
 
     def get_nodes(self):
         return self.nodes
 
     def get_outgoing_links(self, node):
         connections = []
-        for out_node in self.nodes:
-            if self.graph[node].get(out_node, False) != False:
-                connections.append(out_node)
+        if node in self.graph:
+            connections = list(self.graph[node].keys())
         return connections
 
     def value(self, node1, node2):
-        return self.graph[node1][node2]
+        if node1 in self.graph and node2 in self.graph[node1]:
+            return self.graph[node1][node2]
+        return float("inf")
 
     def dijkstra(self, start_node):
         unvisited_nodes = list(self.get_nodes())
@@ -43,14 +30,15 @@ class RoadNetwork(object):
         while unvisited_nodes:
             current_node = None
             for node in unvisited_nodes:
-                if current_node == None:
+                if current_node is None:
                     current_node = node
                 elif shortest_path[node] < shortest_path[current_node]:
                     current_node = node
 
             next_districts = self.get_outgoing_links(current_node)
             for district in next_districts:
-                route = shortest_path[current_node] + self.value(current_node, district)
+                route = shortest_path[current_node] + \
+                    self.value(current_node, district)
                 if route < shortest_path[district]:
                     shortest_path[district] = route
                     previous_nodes[district] = current_node
